7e88b4dcd42df3845d9a6c7056e11c51477e1b28
Sagnik Chakraborty <92094281+thesagnikchakraborty@users.noreply.github.com>
2025-07-08 16:41:17 +0530

    Update server.js

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ const io = new Server(server, {
     }
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 10000;
 
 // --- Data Structures ---
 let rooms = {}; // Stores all game rooms
